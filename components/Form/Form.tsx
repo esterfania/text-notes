3c3d11dddc870f46d689342e8cc8f8f39d2e9978
Ester Gama <esterfaniasantos@gmail.com>
2021-01-10 00:03:07 -0300

    add events on formComponent

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,13 +1,44 @@
-export function Form(): JSX.Element {
-    return (
-        <form>
-            <fieldset>
-                <label placeholder="Título"></label>
-                <input name="titulo" placeholder="Título" />
-                <label placeholder="Insira seu texto"></label>
-                <textarea name="texto" placeholder="Escreva sua nota..." />
-                <button>Criar nota</button>
-            </fieldset>
-        </form>
-    );
+import { ChangeEvent, Component, FormEvent } from 'react';
+
+import { Note } from '../../models/note.model';
+
+export class Form extends Component {
+    private note: Note;
+
+    constructor(props) {
+        super(props);
+        this.note = { titulo: '', texto: '' };
+    }
+
+    handlerCleateNote(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
+    render() {
+        return (
+            <form onSubmit={this.handlerCleateNote.bind(this)}>
+                <fieldset>
+                    <label placeholder="Título"></label>
+                    <input
+                        type="text"
+                        name="titulo"
+                        placeholder="Título"
+                        onChange={this.handlerChangeTitle.bind(this)}
+                    />
+                    <label placeholder="Insira seu texto"></label>
+                    <textarea
+                        name="texto"
+                        placeholder="Escreva sua nota..."
+                        onChange={this.handlerChangeText.bind(this)}
+                    />
+                    <button type="submit">Criar nota</button>
+                </fieldset>
+            </form>
+        );
+    }
+    private handlerChangeTitle(event: ChangeEvent<HTMLInputElement>) {
+        this.note.titulo = event.target.value;
+    }
+    private handlerChangeText(event: ChangeEvent<HTMLTextAreaElement>) {
+        this.note.texto = event.target.value;
+    }
 }
