d91acdcbded5b2482375b5f7986def7d63db99d9
Ester Gama <esterfaniasantos@gmail.com>
2021-01-10 12:00:24 -0300

    refactor Notes

--- components/Notes/Notes.tsx
+++ components/Notes/Notes.tsx
@@ -1,25 +1,13 @@
-import { Component } from 'react';
-
-import { Note } from '../../models/note.model';
 import { Card } from '../card/card';
 
-interface INotesProp {
-    notes: Note[];
-}
-
-export class Notes extends Component<INotesProp> {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <ul>
-                {this.props.notes.map((note, index) => (
-                    <li key={index}>
-                        <Card note={note}></Card>
-                    </li>
-                ))}
-            </ul>
-        );
-    }
+export function Notes({ notes }) {
+    return (
+        <ul>
+            {notes.map((note, index) => (
+                <li key={index}>
+                    <Card note={note}></Card>
+                </li>
+            ))}
+        </ul>
+    );
 }
